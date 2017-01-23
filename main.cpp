93a59ced18ceba638496cc0e1e766238da832f1e
fyyfl <andrei743@mail.ru>
2017-01-23 18:53:47 +0300

    logger2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,115 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include "class_logger.h"
-#include "class_print_console.h"
-#include "class_print_document.h"
+#include <string>
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+class LogAppender;
+
+class Logger
+{
+public:
+	enum LogLevel
+	{
+		LOG_LEVEL_DEFAULT = 0, LOG_LEVEL_ERROR
+	};
+
+public:
+	static Logger* getInstance() {
+		static Logger* loginst;
+		return loginst;
+	}
+		
+public:
+	virtual bool setLogLevel(LogLevel lvl) = 0;
+	virtual bool addAppender(LogAppender *appender) = 0;
+	virtual void LogMessage(Logger::LogLevel level, const std::string &msg) = 0;
+};
+
+
+class LogAppender
+{
+public:
+
+	virtual void LogMessage(Logger::LogLevel level, const std::string &msg) = 0;
+};
+
+
+class FileLogAppender : public LogAppender
+{
+	
+	void setFilename(string file_name)
+	{
+		ifstream file_log(file_name);
+		file_log.close();
+	}
+
+	void LogMessage(Logger::LogLevel level, const std::string &msg)
+	{
+		cout << msg << endl;
+	}
+	
+};
+
+
+class StdLogAppender : public LogAppender
+{
+	void LogMessage(Logger::LogLevel level, const std::string &msg)
+	{
+		cout << msg << endl;
+	}
+};
+
+class LoggerFun : public Logger
+{
+public:
+
+	bool setLogLevel(LogLevel lvl)
+	{
+		if (lvl == 0)
+		{
+			return LogLevel::LOG_LEVEL_DEFAULT;
+		}
+			 
+		else
+		{
+			return LogLevel::LOG_LEVEL_ERROR;
+		}
+
+	}
+
+	bool addAppender(LogAppender *appender)
+	{
+		if (true)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+
+	}
+
+};
+
+#define LOG_DEF(msg) Logger::getInstance()->LogMessage(Logger::LogLevel::LOG_LEVEL_DEFAULT, msg)
+#define LOG_ERR(msg) Logger::getInstance()->LogMessage(Logger::LogLevel::LOG_LEVEL_ERROR, msg)
 
 int main()
 {
-	Logger* ptr[2];
+	Logger* loginst = Logger::getInstance();
 
-	PrintConsole pc;
-	PrintDocument pd;
-	
-	ptr[0] = &pc;
-	ptr[1] = &pd;
+	FileLogAppender *fileAppender = new FileLogAppender;
+	fileAppender.setFilename("qweqwe.log");
 
-	ptr[0]->fun_print_text();
+	loginst->addAppender(fileAppender);
+	loginst->addAppender(new StdLogAppender);
 
-	pd.set_name();
-	pd.get_name();
+	loginst->setLogLevel(Logger::LogLevel::LOG_LEVEL_ERROR); // Log only if LogLevel > LOG_LEVEL_ERROR
 
-	ptr[1]->fun_print_text();
+	LOG_DEF("Logger init def"); // ===>> NOTHIHNG
+	LOG_ERR("Logger init successfully"); //  ====>> std:: ""Logger init successfully""
+										 // file qweqwe.log ====>>> "Logger init successfully"
 
+	return 0;
 }
